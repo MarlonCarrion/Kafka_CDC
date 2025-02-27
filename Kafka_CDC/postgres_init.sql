008c9e82dd93400a805fe3f35310263a0a2913a1
MarlonCarrion <69827251+MarlonCarrion@users.noreply.github.com>
2025-02-27 07:45:00 -0500

    Delete Kafka_CDC directory

--- Kafka_CDC/postgres_init.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE estudiantes (
-    id                  SERIAL PRIMARY KEY,
-    nombre              VARCHAR(100) NOT NULL,
-    apellido            VARCHAR(100) NOT NULL,
-    correo              VARCHAR(150) UNIQUE NOT NULL,
-    fecha_nacimiento    DATE,
-    carrera             VARCHAR(100),
-    semestre            INT
-);
